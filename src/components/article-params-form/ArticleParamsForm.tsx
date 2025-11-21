d3196249b8180464753624674c3f020a9fb82c74
Kirill Lisenko <lisenkokirill2015@yandex.ru>
2025-11-21 16:53:51 +0300

    Добавил состояние для формы(шрифт,цвет шрифта и тд) которые будут применяться к странице при вызове из состояния страницы сеттер которые заранее передан в форму

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,156 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Text } from 'src/ui/text';
+import { useState, useRef, useEffect, FormEvent } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import {
+	fontSizeOptions,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	OptionType,
+	ArticleStateType,
+} from '../../../src/constants/articleProps';
+import { Separator } from 'src/ui/separator/Separator';
 
-export const ArticleParamsForm = () => {
+type PropsArticleParamsForm = {
+	onChange: (data: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = (props: PropsArticleParamsForm) => {
+	const { onChange } = props;
+
+	const asideRef = useRef<HTMLElement | null>(null); // Ссылка на dom aside чтобы добавлять класс открытия и убирать его
+
+	const [isOpenAside, setIsOpenAside] = useState<boolean>(false); // Состояние aside на странице
+
+	const [selectedFont, setSelectedFont] = useState<OptionType>(
+		fontFamilyOptions[0]
+	); // Состояние шрифта
+	const [fontSize, setFontSize] = useState<OptionType>(fontSizeOptions[0]); // Состояние размера шрифта на странице
+	const [fontColor, setFontColor] = useState<OptionType>(fontColors[0]); // Состояние цвета шрифта
+	const [backgroundColor, setBackgroundColor] = useState<OptionType>(
+		backgroundColors[0]
+	); // Состояние цвет фона
+	const [contentWidth, setContentWidth] = useState<OptionType>(
+		contentWidthArr[0]
+	); // Состояние ширины контента
+
+	useEffect(() => {
+		//Открытие и закрытие aside
+		if (isOpenAside) {
+			asideRef.current!.classList.add(styles.container_open);
+		} else {
+			asideRef.current!.classList.remove(styles.container_open);
+		}
+	}, [isOpenAside]);
+
+	//Смена состояние у aside
+	function toggleIsOpen() {
+		setIsOpenAside(!isOpenAside);
+	}
+
+	//Передаёт обьект новых настроек при нажатии на кнопку применить
+	function collectorParams(event: FormEvent) {
+		event.preventDefault();
+		onChange({
+			fontFamilyOption: selectedFont,
+			fontSizeOption: fontSize,
+			fontColor,
+			backgroundColor,
+			contentWidth,
+		});
+	}
+
+	function backToDefaultStyles() {
+		setSelectedFont(fontFamilyOptions[0]),
+			setFontSize(fontSizeOptions[0]),
+			setFontColor(fontColors[0]),
+			setBackgroundColor(backgroundColors[0]),
+			setContentWidth(contentWidthArr[0]);
+		onChange({
+			fontFamilyOption: fontFamilyOptions[0],
+			fontSizeOption: fontSizeOptions[0],
+			fontColor: fontColors[0],
+			backgroundColor: backgroundColors[0],
+			contentWidth: contentWidthArr[0],
+		});
+	}
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				isOpen={isOpenAside}
+				onClick={() => {
+					toggleIsOpen();
+				}}
+			/>
+			<aside className={styles.container} ref={asideRef}>
+				<form
+					className={styles.form}
+					onSubmit={(event) => collectorParams(event)}>
+					<Text
+						as={'h2'}
+						weight={800}
+						size={31}
+						family='open-sans'
+						uppercase={true}>
+						Задайте параметры
+					</Text>
+					<Select
+						options={fontFamilyOptions}
+						selected={selectedFont}
+						title={'Шрифт'}
+						onChange={(value) => {
+							setSelectedFont(value);
+						}}
+					/>
+					<RadioGroup
+						options={fontSizeOptions}
+						selected={fontSize}
+						title={'РАЗМЕР ШРИФТА'}
+						onChange={(value) => {
+							setFontSize(value);
+						}}
+						name={'fontSize'}
+					/>
+					<Select
+						options={fontColors}
+						selected={fontColor}
+						title={'ЦВЕТ ШРИФТА'}
+						onChange={(value) => {
+							setFontColor(value);
+						}}
+					/>
+					<Separator />
+					<Select
+						options={backgroundColors}
+						selected={backgroundColor}
+						title={'ЦВЕТ ФОНА'}
+						onChange={(value) => {
+							setBackgroundColor(value);
+						}}
+					/>
+					<Select
+						options={contentWidthArr}
+						selected={contentWidth}
+						title={'ШИРИНА КОНТЕНТА'}
+						onChange={(value) => {
+							setContentWidth(value);
+						}}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={() => {
+								backToDefaultStyles();
+							}}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,42 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState, useEffect } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
-
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
-
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	//Общие данные страницы которые буду обновляться только после применить или сбросить в  aside
+	const [params, setParams] =
+		useState<typeof defaultArticleState>(defaultArticleState);
+
+	function useNewParametres(data: ArticleStateType) {
+		setParams({ ...data });
+	}
+
+	useEffect(() => {}, [params]);
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': params.fontFamilyOption.value,
+					'--font-size': params.fontSizeOption.value,
+					'--font-color': params.fontColor.value,
+					'--container-width': params.contentWidth.value,
+					'--bg-color': params.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm onChange={useNewParametres} />
 			<Article />
 		</main>
 	);
